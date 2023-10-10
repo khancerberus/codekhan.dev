09e9f31e0eac7040d8e110b8bce4dd0c0653e2a2
Victor Valenzuela <khancerberus@hotmail.cl>
2023-10-10 01:57:29 -0300

    add: Routes to navigate in site

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+import { Navbar } from './components/Navbar'
+import './App.css'
+
 function App() {
   return (
-    <h1>CODEKHAN.DEV</h1>
+    <BrowserRouter basename="/">
+      <Navbar />
+
+      <Routes>
+        <Route path='/' element={<h1>HOME</h1>} />
+        <Route path='/projects' element={<h1>PROYECTOS</h1>} />
+        <Route path='/about' element={<h1>ACERCA DE MI</h1>} />
+
+        <Route path='*' element={<h1>404 NOT FOUND</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
